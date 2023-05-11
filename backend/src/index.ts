401086721677c5d70b6d2b0c0e3eeb2f325641af
Tim Reber <tim.reber@bluewin.ch>
2023-05-12 00:16:44 +0200

    working dagger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,32 +15,20 @@ type Get = {
 
 app.get("/", ({ req, res }: Get) => {
   connect(async (client) => {
+
     // use a node:16-slim container
-    // mount the source code directory from the host at /src in the container
-    const source = client
-      .container()
-      .from("node:16-slim")
-      .withDirectory("/src", client.host().directory("."), {
-        exclude: ["node_modules/", "ci/"],
-      });
-
-    // set the working directory in the container
-    // install application dependencies
-    const runner = source.withWorkdir("/src").withExec(["npm", "install"]);
-
-    // build application
-    // write the build output to the host
-    const buildDir = runner
-      .withExec(["npm", "run", "build"])
-      .directory("./build");
-
-    await buildDir.export("./build");
-    const e = await buildDir.entries();
-
-    console.log("build dir contents:\n", e);
-  });
+    // get version
+    const node = client.container().from("node:16-slim").withExec(["node", "-v"])
+  
+    // execute
+    const version = await node.stdout()
+  
+    // print output
+    console.log("Hello from Dagger and Node " + version)
+  }, { LogOutput: process.stdout })
   res.send("Express + TypeScript Server");
-});
+}
+);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
